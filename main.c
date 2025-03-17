cc6cf5c237e3ffabd598acc8e41cff31efd52686
ParaEstudo <programacao324@gmail.com>
2025-03-17 12:59:46 -0300

    Aula 02 - comando de saida , pular linha
    Criamos o comando de saida , aprendemos a como pular linha , como dar tab dentro do codigo e colocar as aspas no comando

---
+++ main.c
@@ -0,0 +1,31 @@
+/* 
+
+	Objetivo: Apresentar Hello World na tela
+			Exemplo de saída de dados
+	Entrada: N/A
+	Saída: Hello World
+
+*/
+
+#include <stdio.h> // aqui são as bibliotecas padrão da linguagem C
+
+int main() {
+	printf("Hello \nWorld\n");
+	
+	printf("\n\n\n");
+	
+	printf("Hello \n");
+	printf("\"Word \"\n"); // aqui colocamos aspas 
+	
+	printf("Hello \t\t\t World"); // aqui e apreviação de tab 
+	
+	return 0;
+}
+
+// Teste Comentário de uma linha
+
+/*
+Teste de 
+teste para teste 
+onde teset e teste
+*/
